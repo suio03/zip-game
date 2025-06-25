0c5fa0822b417207358eacae9d05e9af8dd19a61
banjo <mr.singledog@gmail.com>
2025-06-25 14:49:25 +0800

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Analytics from "@/components/analytics";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -40,6 +41,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+                    <Analytics />
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
