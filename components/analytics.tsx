adafc774292f0cfc69be6c028b59b35eaa2ce9d8
banjo <mr.singledog@gmail.com>
2025-06-25 14:09:09 +0800

    add analytics

---
+++ components/analytics.tsx
@@ -0,0 +1,46 @@
+import Script from 'next/script'
+
+const Analytics = () => (
+    <>
+        {/* google analytics */}
+        {/* <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-Y0RLKVCNWW"
+        />
+
+
+        <Script
+            id="google-analytics"
+            dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-Y0RLKVCNWW');
+                `,
+            }}
+        />
+        <Script
+            id="microsoft-clarity"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+                __html: `
+                (function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "rplbn184cz");
+        `
+            }}
+        /> */}
+        <Script
+            defer
+            data-domain="zipgame.net"
+            src="https://actone.app/js/script.js"
+        />
+
+    </>
+)
+
+export default Analytics
