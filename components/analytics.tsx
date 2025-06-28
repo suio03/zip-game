2dc311fd9757fcf759ed10cc4f6157a886aad562
banjo <mr.singledog@gmail.com>
2025-06-28 11:00:37 +0800

    add microsoft clarity analytics

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -21,7 +21,7 @@ const Analytics = () => (
                 `,
             }}
         />
-        {/* <Script
+        <Script
             id="microsoft-clarity"
             strategy="afterInteractive"
             dangerouslySetInnerHTML={{
@@ -30,10 +30,10 @@ const Analytics = () => (
                     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                     t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                     y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                })(window, document, "clarity", "script", "rplbn184cz");
+                })(window, document, "clarity", "script", "s6cmyluaa6");
         `
             }}
-        /> */}
+        />
         <Script
             defer
             data-domain="zipgame.net"
